5558cb0d991d56a935aad01aa0f002a33db93903
René Rivas <reinaldorrivas@gmail.com>
2026-03-19 12:53:00 -0400

    fh-solid-clean-code: Minor change

--- src/clean-code/01-names.ts
+++ src/clean-code/01-names.ts
@@ -1,23 +1,23 @@
 interface File {
   id: number;
-  willBeEvaluated: boolean;
+  willBeDeleted: boolean;
 }
 
 export const cleanCodeNames = (): void => {
   // Ejemplo
   // Archivos a evaluar - files to evaluate
   const filesToEvaluate: File[] = [
-    { id: 1, willBeEvaluated: false },
-    { id: 2, willBeEvaluated: false },
-    { id: 3, willBeEvaluated: true },
-    { id: 4, willBeEvaluated: false },
-    { id: 5, willBeEvaluated: false },
-    { id: 7, willBeEvaluated: true },
+    { id: 1, willBeDeleted: false },
+    { id: 2, willBeDeleted: false },
+    { id: 3, willBeDeleted: true },
+    { id: 4, willBeDeleted: false },
+    { id: 5, willBeDeleted: false },
+    { id: 7, willBeDeleted: true },
   ];
 
   // Archivos marcados para borrar - files to delete
   const filesToDelete: File[] = filesToEvaluate.filter(
-    (fileToEvaluate) => fileToEvaluate.willBeEvaluated,
+    (fileToEvaluate) => fileToEvaluate.willBeDeleted,
   );
 
   abstract class User {}
